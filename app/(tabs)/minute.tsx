52223dfbf792adcca5c2880a002237b068bb582a
Jian Wei <itsjianweii@gmail.com>
2026-02-03 00:12:01 +0800

    feat: Setup default screens

---
+++ app/(tabs)/minute.tsx
@@ -0,0 +1,8 @@
+import SwipeFeed from "@/components/minute/SwipeFeed";
+import React from "react";
+
+const Minute = () => {
+  return <SwipeFeed />;
+};
+
+export default Minute;
---
+++ app/(tabs)/partner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Partner = () => {
+  return (
+    <View>
+      <Text>partner</Text>
+    </View>
+  );
+};
+
+export default Partner;
+
+const styles = StyleSheet.create({});
