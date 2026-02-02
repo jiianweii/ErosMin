34279099f8cf06cd4e8403bcf9431f831e4a7e14
Jian Wei <itsjianweii@gmail.com>
2026-02-03 01:20:36 +0800

    fix: UI too high for iOS

--- components/minute/Helper.tsx
+++ components/minute/Helper.tsx
@@ -1,14 +1,29 @@
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { MaterialIcons, SimpleLineIcons } from "@expo/vector-icons";
 import { useTheme } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Helper = () => {
   const { colors } = useTheme();
   return (
     <View style={styles.view}>
       <TouchableOpacity>
-        <MaterialIcons size={24} name="home" color={colors.text} />
+        <MaterialIcons
+          name="keyboard-backspace"
+          size={24}
+          color={colors.text}
+        />
+      </TouchableOpacity>
+      <View style={styles.toggleableView}>
+        <TouchableOpacity style={styles.toggleActive}>
+          <Text style={styles.text}>BEST MATCHES</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.toggle}>
+          <Text style={styles.text}>NEAR YOU</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity>
+        <SimpleLineIcons name="settings" size={24} color={colors.text} />
       </TouchableOpacity>
     </View>
   );
@@ -19,12 +34,35 @@ export default Helper;
 const styles = StyleSheet.create({
   view: {
     position: "absolute",
-    top: 10,
+    top: 50,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
     zIndex: 10,
     width: "80%",
     alignSelf: "center",
     marginVertical: 15,
+  },
+  toggleableView: {
+    flexDirection: "row",
+    padding: 2,
+    backgroundColor: "#000",
+    borderRadius: 9999,
+    overflow: "hidden",
+    gap: 10,
+  },
+  toggleActive: {
+    backgroundColor: "#2e2e2e",
+    borderRadius: 9999,
+    padding: 10,
+  },
+  toggle: {
+    borderRadius: 9999,
+    padding: 10,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: 500,
   },
 });
