abe435659ef1ed56ea1a6020eb07d0521f8b0f6f
Jian Wei <itsjianweii@gmail.com>
2026-02-04 04:43:48 +0800

    refactor: Make navbar lower

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
   view: {
     position: "absolute",
     flexDirection: "row",
-    bottom: 60,
+    bottom: 30,
     alignSelf: "center",
     borderRadius: 30,
     paddingHorizontal: 5,
