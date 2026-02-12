a6da33c0019526bb004abfb958b67bd40aed9337
Jian Wei <itsjianweii@gmail.com>
2026-02-13 02:51:17 +0800

    refactor: Add new screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,23 +9,43 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import NavbarProvider from "@/providers/NavbarProvider";
+import SelectUserProvider from "@/providers/SelectUserProvider";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
+
+// {colorScheme === "dark" ? DarkTheme : DefaultTheme}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <NavbarProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="notification" options={{ headerShown: false }} />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
+      <SelectUserProvider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="archive/archive"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+            <Stack.Screen name="settings" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="auth/register"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="notification"
+              options={{ headerShown: false }}
+            />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </SelectUserProvider>
     </NavbarProvider>
   );
 }
