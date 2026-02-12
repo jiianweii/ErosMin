85c6670cb737be16d0372561446f61fb003b6a8a
Jian Wei <itsjianweii@gmail.com>
2026-02-13 02:54:26 +0800

    refactor: Change hook name

--- util/colors.ts
+++ util/colors.ts
@@ -1,7 +1,7 @@
 import { Colors as Colours } from "@/constants/theme";
 import { useColorScheme } from "react-native";
 
-export default function Colors() {
+export default function useColors() {
   const scheme = useColorScheme();
   const appColors = Colours[scheme ?? "light"];
 
