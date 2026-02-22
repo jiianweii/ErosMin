772eb48fdd561944bc5b983e39656ecf682eb236
Jian Wei <itsjianweii@gmail.com>
2026-02-23 00:14:08 +0800

    feat: Add new screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import NavbarProvider from "@/providers/NavbarProvider";
 import SelectUserProvider from "@/providers/SelectUserProvider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -21,31 +22,55 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <NavbarProvider>
-      <SelectUserProvider>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="archive/archive"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen name="auth/login" options={{ headerShown: false }} />
-            <Stack.Screen name="settings" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="auth/register"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="notification"
-              options={{ headerShown: false }}
-            />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </SelectUserProvider>
-    </NavbarProvider>
+    <GestureHandlerRootView>
+      <NavbarProvider>
+        <SelectUserProvider>
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              {/* Profile */}
+              <Stack.Screen
+                name="profile/archive"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="profile/qrcode"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="profile/settings"
+                options={{ headerShown: false }}
+              />
+
+              {/* Auth */}
+              <Stack.Screen
+                name="auth/login"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="auth/register"
+                options={{ headerShown: false }}
+              />
+              {/* Notification */}
+              <Stack.Screen
+                name="notification"
+                options={{ headerShown: false }}
+              />
+
+              {/* Onboarding */}
+              <Stack.Screen
+                name="introduction"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="setup" options={{ headerShown: false }} />
+              <Stack.Screen name="chatroom" options={{ headerShown: false }} />
+            </Stack>
+            <StatusBar style="auto" />
+          </ThemeProvider>
+        </SelectUserProvider>
+      </NavbarProvider>
+    </GestureHandlerRootView>
   );
 }
